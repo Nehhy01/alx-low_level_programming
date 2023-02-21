9741da86aae4ff5a30fe0f0b5272a1e37189b026
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-21 09:38:24 -0800

    wtf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+	float f;
+	long int li;
+	long long int lli;
+
+	printf("size of an int: %ls.\n", (unsigned long)sizeof(i));
+}
