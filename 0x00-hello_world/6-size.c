96d54cbbfd3aa69ddf3088ac1d0f215ced4faba7
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-22 04:05:39 -0800

    There is a file called C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,4 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of an float: %d byte(s)\n", sizeof(float));
-return (0);
-}
+return (0);}
