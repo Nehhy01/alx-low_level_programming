a528391b49179b97065ceb03d52c081990283b43
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-22 03:21:55 -0800

    There is a file called C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,11 @@
 
 int main(void)
 {
-	int i;
-	double d;
-	char c;
-	float f;
-	long int li;
-	long long int lli;
+printf("size of an char: %ls.\n", (unsigned long)sizeof(char));
+printf("size of an int: %ls.\n", (unsigned long)sizeof(int));
+printf("size of an long int: %ls.\n", (unsigned long)sizeof(long int));
+printf("size of an long long int: %ls.\n", (unsigned long)sizeof(long long int));
+printf("size of an float: %ls.\n", (unsigned long)sizeof(float));
+return (0);
 
-	printf("size of an int: %ls.\n", (unsigned long)sizeof(i));
 }
