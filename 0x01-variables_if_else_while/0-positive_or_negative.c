02903c0652a3560801943e8da181fd8798ba88dd
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-22 20:33:45 -0800

    if, else ans while loop is cool

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+ /**
+ * main - A program that prints n with function printf
+ * Return: 0 (Success)
+ */
+int main(int n;)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		{
+		if (n > 0)
+		{
+		printf("%d is positive\n", n);
+		}
+
+		else if (n < 0)
+		{
+		printf("%d is negative\n", n);
+		}
+
+		else
+		{
+		printf("%d is zero\n", n);
+		}
+		return (0);
+		}
+}
