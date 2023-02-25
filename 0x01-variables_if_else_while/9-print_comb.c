76017f5ae5670b84dbf13783b77b6feae4889b39
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-25 15:38:45 -0800

    if, else ans while loop is cool

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,19 +2,22 @@
 
 /**
  * main - Entry point
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
-}
+{
 	int a;
 
-	for (a = 0; a < 10; a++) {
+	for (a = 0; a < 10; a++)
+	{
 		putchar(a + '0');
-			if ( a < 9)
-				putchar(',');
-				putchar( );
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
