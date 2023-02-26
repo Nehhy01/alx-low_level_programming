f7c324ec72a1c5dcf2fcf5ee3e88a11b94b8fe7e
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-25 17:16:04 -0800

    if, else ans while loop is cool

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - Prints all combo of 2 2 digit nunbers.
- * Returm: Always 0 (Success)
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ * ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
