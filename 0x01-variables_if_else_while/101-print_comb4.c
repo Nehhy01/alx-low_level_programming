613010920242ddc7292bd1e736820a34aa941ef4
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-25 16:34:00 -0800

    if, else ans while loop is cool

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c, d, e;
+		for (c = 0; c < 8; c++)
+	{
+		for (d = c + 1; d < 9; d++)
+		{
+		for (e = d + 1; e < 10; e++)
+		{
+		putchar((c % 10) + '0');
+		putchar((d % 10) + '0');
+		putchar((e % 10) + '0');
+
+		if (c == 7 && d == 8 && e == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
+		}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
