2bc57d6c10abf12a88cd985359684e534b169952
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-26 19:33:07 -0800

    Functions and nested loops is fun

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - prints the absolute value of an integer
+ * Return: - 0 (success)
+ * @a: Parameter to be commuted.
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		_putchar(a + '0');
+	else
+		_putchar(a + '0');
+
+return (0);
+}
