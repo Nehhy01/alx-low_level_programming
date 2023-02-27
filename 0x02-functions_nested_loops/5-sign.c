4e154da3bf0071bf6af91112860c5dd72f2d632d
Nehhy01 <olanehemiah2006@gmail.com>
2023-02-26 19:02:13 -0800

    Functions and nested loops is fun

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  print_sign - prints + 0 or - if n is positive, zero or negative.
+ *  @n: parameter to be checked.
+ *  Return: 1 if n is positive 0 if its zero or -1 if its negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+
+	if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+
+	else
+	-putchar(0);
+	return (0);
+}
