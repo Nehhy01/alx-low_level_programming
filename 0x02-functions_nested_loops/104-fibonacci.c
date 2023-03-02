235c78753915d81976c915cabbd79fa5c6d57efe
Nehhy01 <olanehemiah2006@gmail.com>
2023-03-01 20:07:54 -0800

    Functions and nested loops is fun

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -39,10 +39,10 @@ for (count = 0; count < 92; count++)
 
 	if (count != 98)
 		printf(", ");
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+	fib1_half1 = fib2_half1;
+	fib1_half2 = fib2_half2;
+	fib2_half1 = half1;
+	fib2_half2 = half2;
 	}
 	printf("\n");
 	return (0);
